9af573311e3d8b92aecc9381441286beb5dc381b
Igor Packo <foxyzv@gmail.com>
2018-11-23 23:15:59 +0100

    C2 - 2.1 added api for lists (add, push)

--- 2-linked-lists/2.0-helper.js
+++ 2-linked-lists/2.0-helper.js
@@ -1,43 +1,71 @@
 module.exports = {
-  LinkedList: function(value) {
-    this.value = value;
-    this.next = null;
-  },
+  LinkedList: LinkedList
+};
 
-  printList: function(start) {
-    let node = start;
-    console.log("** list start **");
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
 
-    while (node !== null) {
-      console.log(node.value);
-      node = node.next;
-    }
+function Node(value, next) {
+  this.value = value;
+  this.next = next;
+}
 
-    console.log("** list end **");
-  },
+LinkedList.prototype.printList = function(start) {
+  let node = start;
+  console.log("** list start **");
 
-  removeDuplicates: function(start) {
-    // no need to delete anything
-    if (start === null || start.next === null) {
-      return start;
-    }
-
-    let currentNode = start;
-    let nextNode = start.next;
-    let map = [];
-
-    map[currentNode.value] = true;
-
-    while (nextNode !== null) {
-      const value = nextNode.value;
-
-      if (map[value]) {
-        currentNode.next = nextNode.next;
-      } else {
-        map[value] = true;
-        currentNode = nextNode;
-      }
-      nextNode = nextNode.next;
-    }
+  while (node !== null) {
+    console.log(node.value + " next -> " + node.next);
+    node = node.next;
   }
+
+  console.log("** list end **");
+};
+
+LinkedList.prototype.addToHead = function(value) {
+  const newNode = new Node(value, this.head);
+  if (!this.head) this.tail = newNode;
+  this.head = newNode;
+};
+
+LinkedList.prototype.pushToTail = function(value) {
+  const newNode = new Node(value, null);
+
+  if (!this.head) {
+    this.head = newNode;
+    this.tail = newNode;
+    return;
+  }
+
+  this.tail.next = newNode;
+  this.tail = newNode;
+};
+
+LinkedList.prototype.removeDuplicates = function(start) {
+  // no need to delete anything
+  if (start === null || start.next === null) {
+    return start;
+  }
+
+  let currentNode = start;
+  let nextNode = start.next;
+  let map = [];
+
+  map[currentNode.value] = true;
+
+  while (nextNode !== null) {
+    const value = nextNode.value;
+
+    if (map[value]) {
+      currentNode.next = nextNode.next;
+    } else {
+      map[value] = true;
+      currentNode = nextNode;
+    }
+    nextNode = nextNode.next;
+  }
+
+  this.tail = currentNode;
 };
